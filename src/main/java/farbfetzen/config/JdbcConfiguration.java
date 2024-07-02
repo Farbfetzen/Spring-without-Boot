6301c4ad87b4c4110bc2a062d64828100696e12c
Farbfetzen <farbfetzen@posteo.de>
2024-07-02 20:14:38 +0200

    Read datasource url from properties file

--- src/main/java/farbfetzen/config/JdbcConfiguration.java
+++ src/main/java/farbfetzen/config/JdbcConfiguration.java
@@ -2,18 +2,21 @@ package farbfetzen.config;
 
 import javax.sql.DataSource;
 import org.h2.jdbcx.JdbcDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.simple.JdbcClient;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class JdbcConfiguration {
 
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource(@Value("${datasource.url}") final String dataSourceUrl) {
         // The H2 database driver creates the database if it does not exist.
         final var dataSource = new JdbcDataSource();
-        dataSource.setUrl("jdbc:h2:file:./devDb");
+        dataSource.setUrl(dataSourceUrl);
         return dataSource;
     }
 
