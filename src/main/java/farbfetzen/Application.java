c52bc3415ac2d88281b540569bda4ad17657764a
Farbfetzen <farbfetzen@posteo.de>
2024-07-03 20:23:50 +0200

    Import the beans via an annotation

--- src/main/java/farbfetzen/Application.java
+++ src/main/java/farbfetzen/Application.java
@@ -1,12 +1,14 @@
 package farbfetzen;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource("beans.xml")
 @Slf4j
 public class Application {
 
     public static void main(final String[] args) {
-        new ClassPathXmlApplicationContext("beans.xml");
+        new AnnotationConfigApplicationContext(Application.class);
     }
 }
